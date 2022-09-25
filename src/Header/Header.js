0d8b6b185dd48d4ce9d9a100cb372c76cd151640
parteekjain99 <parteekgarg069@gmail.com>
2022-09-25 19:28:01 +0530

    final

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,35 +1,3 @@
-// import React from 'react'
-// import { Link } from 'react-router-dom'
-// import "../Header/Header.css"
-// import { Use_r_state } from '../Store/Store'
-// const Header = () => {
-//   const {x :{cart}} = Use_r_state();
-//   return (
-//     <>
-//     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//   <Link className="navbar-brand" to="/">Global</Link>
-//   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//     <span className="navbar-toggler-icon"></span>
-//   </button>
-
-//   <div className="collapse navbar-collapse" id="navbarSupportedContent">
-//     <ul className="navbar-nav mr-auto">
-//       <li className="nav-item active">
-//         <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
-//       </li>
-      
-      
-     
-//     </ul>
-//    <Link to="/Cart"><i className="fa-sharp fa-solid fa-cart-shopping cart_icon" data={cart && cart.length}></i></Link> 
-//   </div>
-// </nav>
-//     </>
-//   )
-// }
-
-// export default Header
-
 import React from "react";
 import "../Header/Header.css"
 
--- src/Product/Product.js
+++ src/Product/Product.js
@@ -1,39 +1,18 @@
 import React, {  useState } from 'react'
 import Cards from '../card/Card';
  import "../Product/Product.css"
-// import { Use_r_state } from '../Store/Store'
+
 import data from "../data/Data"
 const Product = ({ handleClick }) => {
     
     
-    // const [ x,dis] = Use_r_state();
-// 
     const [list , setdemo] = useState(data);
-
-      
-   
-    // const allp = async () => {
-    //     const res = await fetch('https://dummyjson.com/products');
-    //     const data = await res.json();
-    //     setdemo(data.products)
-    // }
-    
-    //  useEffect( () => {
-    //     allp();
-        
-    //  },[])
-
     const filterResult =  (cartitem) => {
        const result = data.filter((cur) => {
         return cur.size === cartitem
        });
        setdemo(result)
     }
-
-
-   
-
-
     return (
     <>
      
@@ -41,12 +20,13 @@ const Product = ({ handleClick }) => {
      <div className="cointainer-fluid mx-2">
         <div className="row mt-5 mx-2">
           <div className="col-md-3">
-          <button type="button" className="btn btn-secondary w-100 mb-4" onClick={() => setdemo(data)}>All</button>
-          <button type="button" className="btn btn-secondary w-100 mb-4" onClick={() => filterResult("S")}>S</button>
-          <button type="button" className="btn btn-secondary w-100 mb-4" onClick={() => filterResult("L")}>L</button>
-          <button type="button" className="btn btn-secondary w-100 mb-4" onClick={() => filterResult("XL")}>XL</button>
-          <button type="button" className="btn btn-secondary w-100 mb-4" onClick={() => filterResult("M")}>M</button>
-          <button type="button" className="btn btn-secondary w-100 mb-4" onClick={() => filterResult("XXL")}>XXL</button>
+          <button type="button" className="btn btn-secondary w-15 mb-4 m-3"  onClick={() => setdemo(data)}>All</button>
+          <button type="button" className="btn btn-secondary w-15 mb-4 m-3"  onClick={() => filterResult("S")}>S</button>
+          <button type="button" className="btn btn-secondary w-15 mb-4 m-3"  onClick={() => filterResult("L")}>L</button>
+          <button type="button" className="btn btn-secondary w-15 mb-4 m-3"  onClick={() => filterResult("XL")}>XL</button>
+          <button type="button" className="btn btn-secondary w-15 mb-4 m-3"  onClick={() => filterResult("M")}>M</button>
+          <button type="button" className="btn btn-secondary w-15 mb-4 m-3"  onClick={() => filterResult("XXL")}>XXL</button>
+
           </div>
           <div className="col-md-9">
             <div className="row">
